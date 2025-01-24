36f6e56cccb13d9666f23641ab6cd35d3e2fa514
Sardina Jose Adrian <you@example.com>
2025-01-24 16:07:29 -0300

    subiendo movimientos basicos del personaje

---
+++ Reino Burbuja/Assets/Scripts/RotateCharacter.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class RotateCharacter : MonoBehaviour
+{
+    private void Update()
+    {
+        Vector3 objective = MousePosition();
+        objective.y = transform.position.y;
+        Debug.Log(MousePosition());
+        transform.LookAt(objective);
+    }
+
+    public static Vector3 MousePosition()
+    {
+        Vector3 mousePos = new Vector3(0, 0, 0);
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit raycastHit;
+        if (Physics.Raycast(ray, out raycastHit))
+        {
+            mousePos = raycastHit.point;
+            return mousePos;
+        }
+        return mousePos;
+    }
+
+}
