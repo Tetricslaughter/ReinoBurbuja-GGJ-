3b3c397f8de388640b8a183097a59138aec13e7d
RafaelFascio <rafaelfascio@gmail.com>
2025-01-26 18:02:58 -0300

    ff

--- Reino Burbuja/Assets/Scripts/Weapon/Pistol.cs
+++ Reino Burbuja/Assets/Scripts/Weapon/Pistol.cs
@@ -6,6 +6,7 @@ public class Pistol : Weapon
 {
 
     [SerializeField] ParticleSystem particleSystem;
+    public SonidoDisparo sonidoDisparo;
    
     float nextShot;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -26,7 +27,7 @@ public class Pistol : Weapon
         {
             nextShot = Time.time + fireRate;
             Shoot();
-            
+            sonidoDisparo.PlayClip();
         }
         if (Input.GetMouseButtonUp(0))
         {
